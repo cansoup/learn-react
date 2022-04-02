dc1883d18b0c213d16a07a1947ac572b96495d73
cansoup <0311minha@gmail.com>
2022-04-02 18:24:20 +0900

    0402: Webpack 설정

--- lecture/02끝말잇기/webpack.config.js
+++ lecture/02끝말잇기/webpack.config.js
@@ -1,3 +1,30 @@
+const path = require('path');
+
 module.exports = {
+  name: 'word-relay-setting',
+  mode: 'development', // 실서비스: Production
+  devtool: 'eval',
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
   
+  entry: { // 입력
+    app: ['./client'],
+  },
+
+  module: {
+    rules: [{
+      test: /\.jsx?/,
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env', '@babel/preset-react'],
+        plugins: ['@babel/plugin-proposal-class-properties'],
+      }
+    }]
+  },
+
+  output: { // 출력
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.js'
+  },
 }
