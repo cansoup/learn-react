ba394f88d5e07d3baf546c625ec1be696674944b
cansoup <0311minha@gmail.com>
2022-04-24 20:01:44 +0900

    0424: memo로 최적화하기

--- lecture/07틱택토/Td.jsx
+++ lecture/07틱택토/Td.jsx
@@ -1,7 +1,18 @@
-import React, { useCallback } from 'react';
+import React, { memo, useCallback, useEffect, useRef } from 'react';
 import { CLICK_CELL, SET_TURN } from './TicTacToe';
 
-const Td = ({rowIndex, cellIndex, dispatch, cellData}) => {
+const Td = memo(({rowIndex, cellIndex, dispatch, cellData}) => {
+  console.log('td rendered');
+  // 최적화하지 않았을 때 변화가 없는 Td가 다시 render된 이유를 아래의 방법을 통해 알아낼 수 있다.
+  const ref = useRef([]);
+  useEffect(() => {
+    // 값이 false인 요소 때문에 render가 다시 실행된다.
+    // 이 경우 cellData가 render가 다시 실행되는 원인이었다.
+    // memo를 사용하면 변하지 않은 나머지 td들이 모두 다시 render되는 것을 막을 수 있다.
+    // console.log(rowIndex === ref.current[0], cellIndex === ref.current[1], dispatch === ref.current[2], cellData === ref.current[3])
+    ref.current = [rowIndex, cellIndex, dispatch, cellData];
+  }, [rowIndex, cellIndex, dispatch, cellData]);
+
   const onClickTd = useCallback(() => {
     if(cellData) {
       return;
@@ -15,6 +26,6 @@ const Td = ({rowIndex, cellIndex, dispatch, cellData}) => {
     <td onClick={onClickTd}>{cellData}</td>
   )
   
-}
+})
 
 export default Td;
--- lecture/07틱택토/Tr.jsx
+++ lecture/07틱택토/Tr.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, {memo} from 'react';
 import Td from './Td';
 
-const Tr = ({ rowData, rowIndex, dispatch }) => {
+// memo를 사용해도 최적화가 안될 때 최후의 수단으로 useMemo를 사용할 수 있다.
+// useMemo로 컴포넌트 자체를 기억하여 변화가 있을 때만 render하도록 한다.
+const Tr = memo(({ rowData, rowIndex, dispatch }) => {
+  console.log('tr rendered');
+
   return (
     <tr>
       { Array(rowData.length).fill().map((td, i) =>  (<Td rowIndex={rowIndex} cellIndex={i} dispatch={dispatch} key={`td-${i}`} cellData={rowData[i]} >{'1'}</Td>)) }
     </tr>
   )
   
-}
+});
 
 export default Tr;
